4862294ad859ff8866ff1acc4516840cc59ac314
aneesafaisal <aneesa.amdfaisal@icloud.com>
2025-11-21 09:34:16 +0100

    up

--- Modele/Joueur.class.php
+++ Modele/Joueur.class.php
@@ -1,8 +1,6 @@
 <?php
 class joueur {
 
-    require_once "r301php2025_db";
-
     private $numero_licence;
     private $statut;
     private $commentaire;
@@ -23,21 +21,68 @@ class joueur {
         $this->poids = $poids;
     }
 
-    public static function getAll() {
-        $pdo = Database::connect();
-        $stmt = $pdo->query("SELECT * FROM Joueur");
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function getNumeroLicence() {
+        return $this->numero_licence;
     }
 
-    public function save() {
-        $pdo = Database::connect();
-        $sql = "INSERT INTO Joueur VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([
-            $this->numero_licence, $this->statut, $this->commentaire,
-            $this->nom, $this->prenom, $this->date_naissance,
-            $this->taille, $this->poids
-        ]);
+    public function setNumeroLicence($n) {
+        $this->numero_licence = $n;
+    }
+
+    public function getStatut() {
+        return $this->statut;
+    }
+
+    public function setStatut($s) {
+        $this->statut = $s;
+    }
+
+    public function getCommentaire() {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire($c) {
+        $this->commentaire = $c;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function setNom($n) {
+        $this->nom = $n;
+    }
+
+    public function getPrenom() {
+        return $this->prenom;
+    }
+
+    public function setPrenom($p) {
+        $this->prenom = $p;
+    }
+
+    public function getDateNaissance() {
+        return $this->date_naissance;
+    }
+
+    public function setDateNaissance($d) {
+        $this->date_naissance = $d;
+    }
+
+    public function getTaille() {
+        return $this->taille."cm";
+    }
+
+    public function setTaille($t) {
+        $this->taille = $t;
+    }
+
+    public function getPoids() {
+        return $this->poids;
+    }
+
+    public function setPoids($p) {
+        $this->poids = $p."kg";
     }
 
 }
