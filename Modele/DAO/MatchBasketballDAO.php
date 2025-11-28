e7dd1b842c3aa32157abcf6c6984bde28c2c5a4a
koshuanair <koshua.nair@etu.iut-tlse3.fr>
2025-11-28 09:10:16 +0100

    DAOBasketba;ll

--- Modele/DAO/MatchBasketballDAO.php
+++ Modele/DAO/MatchBasketballDAO.php
@@ -5,7 +5,7 @@
     public function __construct() {
         try {
             $db = 'r301php2025_db';
-            $server = 'http://mysql.alwaysdata.com/';
+            $server = 'mysql.alwaysdata.com/';
             $login = '442017';
             $mdp = '!@#$1234abcd';
             $this->linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp); 
@@ -18,16 +18,141 @@
 
     public function getNbMatch ($result){
         $req = $this->linkpdo->prepare('SELECT count(*) as nb from Match_Basketball where Resultat = :resultat');
-        $req->execute(array('resultat => $result'));
-        $nb = $req->fetch();
+        $req->execute(array(":resultat" => $result));
+        $nb = $req->fetch(PDO::FETCH_ASSOC);
         return $nb['nb'];
     }
 
     public function getTauxDeVictoire(){
         $gagne = $this->getNbMatch("Victoire");
         $req = $this->linkpdo->query('SELECT count(*) as nb from Match_Basketball');
-        $row = $req->fetch();
+        $row = $req->fetch(PDO::FETCH_ASSOC);
         $total = $row['nb'];
         return ($gagne/$total) * 100;
     }
+
+    public function getNbMatchCeMois(){
+    $req = $this->linkpdo->prepare(
+        'SELECT COUNT(*) AS nb
+         FROM Match_Basketball
+         WHERE MONTH(DateDeMatch) = MONTH(CURDATE())
+         AND YEAR(DateDeMatch) = YEAR(CURDATE())'
+    );    
+    $req->execute();
+    $row = $req->fetch(PDO::FETCH_ASSOC);
+    return $row['nb'];
     }
+
+    public function getAllMatches(){
+        $req = $this->linkpdo->query("SELECT * FROM Match_Basketball");
+        $matches = [];
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+        $matches[] = new MatchBasketball(
+            $row['DateDeMatch'],
+            $row['HeureDeMatch'],
+            $row['NomEquipeAdversaire'],
+            $row['LieuDeRencontre'],
+            $row['Resultat'],
+            $row['PointsMarquesParAdversaire'],
+            $row['Statut']
+        );
+    }
+
+    return $matches;
+    }
+
+    public function insertMatch($date, $heure, $equipe, $lieu, $resultat, $pointsAdv, $statut) {
+     $req = $this->linkpdo->prepare(
+        'INSERT INTO Match_Basketball
+        (DateDeMatch, HeureDeMatch, NomEquipeAdversaire, LieuDeRencontre, Resultat, PointsMarquesParAdversaire, Statut)
+        VALUES (:date, :heure, :equipe, :lieu, :resultat, :pointsAdv, :statut)'
+    );
+    return $req->execute(array(
+        ":date"     => $date,
+        ":heure"    => $heure,
+        ":equipe"   => $equipe,
+        ":lieu"     => $lieu,
+        ":resultat" => $resultat,
+        ":pointsAdv"=> $pointsAdv,
+        ":statut"   => $statut
+    ));
+    }
+
+    public function updateMatch(
+    $dateAncien, $heureAncien,
+    $dateNouvelle, $heureNouvelle,
+    $nomEquipe,
+    $lieu,
+    $resultat,
+    $points,
+    $statut
+    ) {
+    $sql = 'UPDATE Match_Basketball
+            SET DateDeMatch = :newDate,
+                HeureDeMatch = :newTime,
+                NomEquipeAdversaire = :nomEquipe,
+                LieuDeRencontre = :lieu,
+                Resultat = :resultat,
+                PointsMarquesParAdversaire = :points,
+                Statut = :statut
+            WHERE DateDeMatch = :oldDate
+              AND HeureDeMatch = :oldTime';
+
+    $req = $this->linkpdo->prepare($sql);
+
+    return $req->execute(array(
+    ':newDate'   => $dateNouvelle,
+    ':newTime'   => $heureNouvelle,
+    ':nomEquipe' => $nomEquipe,
+    ':lieu'      => $lieu,
+    ':resultat'  => $resultat,
+    ':points'    => $points,
+    ':statut'    => $statut,
+    ':oldDate'   => $dateAncien,
+    ':oldTime'   => $heureAncien
+    ));
+
+}
+
+public function updateStatut($date, $heure, $nouveauStatut) {
+    $req = $this->linkpdo->prepare(
+        "UPDATE Match_Basketball 
+         SET Statut = :statut
+         WHERE DateDeMatch = :d AND HeureDeMatch = :heure"
+    );
+
+    return $req->execute([
+        ":statut" => $nouveauStatut,
+        ":d"   => $date,
+        ":heure"  => $heure
+    ]);
+
+}
+
+public function updateResultat($date, $heure, $nouveauRes) {
+    $req = $this->linkpdo->prepare(
+        "UPDATE Match_Basketball 
+         SET Resultat = :resultat
+         WHERE DateDeMatch = :d AND HeureDeMatch = :heure"
+    );
+
+    return $req->execute(array(
+        ":resultat" => $nouveauRes,
+        ":d"   => $date,
+        ":heure"  => $heure
+    ));
+
+}
+
+public function deleteMatch($date, $heure){
+    $req = $this->linkpdo->prepare('DELETE FROM Match_Basketball WHERE DateDeMatch = :d AND HeureDeMatch = :h');
+    return $req->execute(array('d' => $date, 'h' => $heure));
+}
+
+
+
+
+}
+
+
+?>
